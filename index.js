41e8d134997d71d2b042282c98c48fa73b64edf2
FelixLusseau <felix.lusseau@gmail.com>
2023-03-20 00:51:49 +0100

    cleaner and embeds

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { EmbedBuilder } = require('discord.js');
 const Chart = require("chart");
 const { ClashRoyaleAPI } = require('@varandas/clash-royale-api')
 require("dotenv").config();
@@ -20,61 +21,81 @@ bot.on("messageCreate", async (message) => {
     var Datas = [];
     api.getClanCurrentRiverRace("#YRLJGL9")
       .then((response) => {
-        for (let i = 0; i < response.clans.length; i++) {
-          var labels = response.clans[i].name
-          Labels.push(labels);
-          var data = response.clans[i].periodPoints
-          Datas.push(data);
-          //console.log(response.clans[i].name)
-          //console.log(response.clans[i].periodPoints)
-        }
-        // console.log(response.clan)
-        // console.log(Labels)
-        // console.log(Datas)
-        const chart = {
-          type: 'horizontalBar',
-          data: {
-            labels: Labels,
-            datasets: [
-              {
-                label: 'Medals',
-                data: Datas,
-                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)'],//'rgba(54,255,51,0.2)',
-                borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)'], //'#33FF3F',
-                borderWidth: 1,
-              },
-            ],
-          },
-          options: {
-            'scales':
-            {
-              'xAxes': [
-                {
-                  'ticks':
-                  {
-                    'beginAtZero': true,
-                    'max': 40000,
-                  }
-                }
-              ]
-            }
-          }
-        };
-        const encodedChart = encodeURIComponent(JSON.stringify(chart));
-        const chartUrl = `https://quickchart.io/chart?c=${encodedChart}`;
-        //console.log(chart)
-        try {
-          message.channel.send(`${chartUrl}`);
-
-        } catch (e) {
-          console.log(e);
-        }
+        return response
       })
       .catch((err) => {
         console.log("CR-API error : ", err)
       })
+    let RiverRace = await api.getClanCurrentRiverRace("#YRLJGL9")
+    for (let i = 0; i < RiverRace.clans.length; i++) {
+      var labels = RiverRace.clans[i].name
+      Labels.push(labels);
+      var data = RiverRace.clans[i].periodPoints
+      Datas.push(data);
+      //console.log(RiverRace.clans[i].name)
+      //console.log(RiverRace.clans[i].periodPoints)
+    }
+    // console.log(RiverRace.clan)
+    // console.log(Labels)
+    // console.log(Datas)
+    const chart = {
+      type: 'horizontalBar',
+      data: {
+        labels: Labels,
+        datasets: [
+          {
+            label: 'Medals',
+            data: Datas,
+            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)'],//'rgba(54,255,51,0.2)',
+            borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)'], //'#33FF3F',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        'scales':
+        {
+          'xAxes': [
+            {
+              'ticks':
+              {
+                'beginAtZero': true,
+                'max': 45000,
+              }
+            }
+          ]
+        }
+      }
+    };
+    const encodedChart = encodeURIComponent(JSON.stringify(chart));
+    const chartUrl = `https://quickchart.io/chart?c=${encodedChart}`;
+    //console.log(chart)
+    try {
+      const medalsEmbed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle('Current river race')
+        //.setURL('https://discord.js.org/')
+        .setAuthor({ name: bot.user.tag, iconURL: bot.user.avatar /* , url: 'https://discord.js.org' */ })
+        //.setDescription(chartUrl)
+        .setThumbnail('https://cdn.discordapp.com/attachments/527820923114487830/1071116873321697300/png_20230203_181427_0000.png')
+        /* .addFields(
+          { name: 'Regular field title', value: 'Some value here' },
+          { name: '\u200B', value: '\u200B' },
+          { name: 'Inline field title', value: 'Some value here', inline: true },
+          { name: 'Inline field title', value: 'Some value here', inline: true },
+        )
+        .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })*/
+        .setImage(chartUrl)
+        .setTimestamp()
+        .setFooter({ text: 'by OPM | Féfé ⚡', iconURL: 'https://avatars.githubusercontent.com/u/94113911?s=400&v=4' });
+
+      message.channel.send({ embeds: [medalsEmbed] });
+      //message.channel.send(`${chartUrl}`);
+    } catch (e) {
+      console.log(e);
+    }
   }
-  else if (message.content === "cc") {
+  else if (message.content === "!attacks") {
     var Players = "";
     api.getClanCurrentRiverRace("#YRLJGL9")
       .then((response) => {
@@ -101,13 +122,33 @@ bot.on("messageCreate", async (message) => {
           //console.log(members[i].name)
           //console.log(RiverRace.clan.participants[j].decksUsedToday)
           let decksUsedToday = 4 - RiverRace.clan.participants[j].decksUsedToday
-          Players += members[i].name + " : " + decksUsedToday + " restant" + (decksUsedToday > 1 ? "s" : "") + "\n";
+          Players += members[i].name + " : " + decksUsedToday + " remaining" + /* (decksUsedToday > 1 ? "s" : "") + */ "\n";
         }
       }
     }
     //console.log(Players)
     if (Players != "") {
-      message.channel.send(Players);
+      Players += "\n" + "https://royaleapi.com/clan/YRLJGL9" + "\n"
+      const attacksEmbed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle('Attacks remaining')
+        //.setURL('https://discord.js.org/')
+        .setAuthor({ name: bot.user.tag, iconURL: bot.user.avatar /* , url: 'https://discord.js.org' */ })
+        .setDescription(Players)
+        .setThumbnail('https://cdn.discordapp.com/attachments/527820923114487830/1071116873321697300/png_20230203_181427_0000.png')
+        /* .addFields(
+          { name: 'Regular field title', value: 'Some value here' },
+          { name: '\u200B', value: '\u200B' },
+          { name: 'Inline field title', value: 'Some value here', inline: true },
+          { name: 'Inline field title', value: 'Some value here', inline: true },
+        )
+        .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+        .setImage('https://i.imgur.com/AfFp7pu.png') */
+        .setTimestamp()
+        .setFooter({ text: 'by OPM | Féfé ⚡', iconURL: 'https://avatars.githubusercontent.com/u/94113911?s=400&v=4' });
+
+      message.channel.send({ embeds: [attacksEmbed] });
+      //message.channel.send(Players);
       //message.channel.send("<@UserID>");
     }
   }

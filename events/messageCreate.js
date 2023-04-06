2b8fa78303268b10ee9c490b6f678c33f9ff8e5e
FelixLusseau <felix.lusseau@gmail.com>
2023-04-06 23:25:10 +0200

    Tag detect2

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,7 +6,7 @@ module.exports = {
         //console.log('messageCreate event triggered.');
         if (message.author.tag == bot.user.tag) return;
         //console.log(message.content);
-        const regex = /\#[a-zA-Z0-9]{8,9}$/g
+        const regex = /\#[a-zA-Z0-9]{8,9}\b/g
         //console.log(message.content.search(regex))
         if (message.content.search(regex) >= 0) {
             let url = "https://royaleapi.com/player/" + message.content.match(regex)[0].substring(1)

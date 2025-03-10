dbcf49c8945f6af6c91c9b3e9b4ee352a404f065
FelixLusseau <felix.lusseau@gmail.com>
2025-03-10 23:58:02 +0100

    ffavg option for current players only and decksUsed begin

--- commands/ffavg.js
+++ commands/ffavg.js
@@ -13,7 +13,10 @@ module.exports = {
                 .setRequired(true))
         .addIntegerOption(option =>
             option.setName('limit')
-                .setDescription('Max weeks to check (from 1 to 10)')),
+                .setDescription('Max weeks to check (from 1 to 10)'))
+        .addBooleanOption(option =>
+            option.setName('include_all_players')
+                .setDescription('Include players curently out of the clan')),
 
     async execute(bot, api, interaction) {
         await interaction.deferReply({ ephemeral: false });
@@ -21,8 +24,10 @@ module.exports = {
         if (functions.isRegisteredClan(bot, interaction, interaction.channel, clan) == false) // Check if the clan is registered
             return
         const limit = interaction.options.getInteger('limit');
+        const include_all_players = interaction.options.getBoolean('include_all_players');
 
         let avg = await functions.fetchHist(clan.substring(1)); // Get the clans' score history
+        // console.log(JSON.stringify(avg));
         let avgArray = {};
         let seasonId = avg.items[0].seasonId;
         // if (avg.items[0].sectionIndex < 3)
@@ -34,10 +39,13 @@ module.exports = {
             if (avg.items[0].standings[h].clan.tag == clan) {
                 for (let p = 0; p < avg.items[0].standings[h].clan.participants.length; p++) {
                     let participant = avg.items[0].standings[h].clan.participants[p];
-                    avgArray[participant.name] = {};
-                    avgArray[participant.name]['fame'] = participant.fame;
-                    avgArray[participant.name]['array'] = [participant.fame, null, null, null, null, null, null, null, null, null];
-                    avgArray[participant.name]['count'] = 1;
+                    if (participant.fame > 0 || include_all_players) {
+                        avgArray[participant.name] = {};
+                        avgArray[participant.name]['fame'] = participant.fame;
+                        avgArray[participant.name]['array'] = [participant.fame, null, null, null, null, null, null, null, null, null];
+                        avgArray[participant.name]['count'] = 1;
+                        avgArray[participant.name]['decksUsed'] = [participant.decksUsed, null, null, null, null, null, null, null, null, null];
+                    }
                 }
             }
         }
@@ -53,19 +61,25 @@ module.exports = {
                 if (avg.items[p].standings[h].clan.tag == clan) {
                     for (let q = 0; q < avg.items[p].standings[h].clan.participants.length; q++) {
                         let participant = avg.items[p].standings[h].clan.participants[q];
-                        if (!(participant.name in avgArray)) {
+                        if (!(participant.name in avgArray) && include_all_players) {
                             avgArray[participant.name] = {};
+                        }
+                        else if (!(participant.name in avgArray)) {
+                            continue;
                         }
                         if (avgArray[participant.name]['fame'] > 0) {
                             avgArray[participant.name]['fame'] += participant.fame;
                             avgArray[participant.name]['array'][p] = participant.fame;
                             avgArray[participant.name]['count'] += 1;
+                            avgArray[participant.name]['decksUsed'][p] = participant.decksUsed;
                         }
                         else {
                             avgArray[participant.name]['fame'] = participant.fame;
                             avgArray[participant.name]['array'] = [null, null, null, null, null, null, null, null, null, null];
                             avgArray[participant.name]['array'][p] = participant.fame;
                             avgArray[participant.name]['count'] = 1;
+                            avgArray[participant.name]['decksUsed'] = [null, null, null, null, null, null, null, null, null, null];
+                            avgArray[participant.name]['decksUsed'][p] = participant.decksUsed;
                         }
                     }
                 }
@@ -82,6 +96,9 @@ module.exports = {
         for (const [key, value] of Object.entries(sortedAvgObject)) {
             avgArray[key]['fame'] = value['fame'] / value['count'];
         }
+
+        // console.log(sortedAvgObject);
+
         await functions.excel(sortedAvgObject)
         const excel = new AttachmentBuilder('averages.xlsx');
         const png = new AttachmentBuilder('averages.png');
